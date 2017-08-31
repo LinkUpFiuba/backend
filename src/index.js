b327d9c63327977ded22dce05d9fb0b50d8290be
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-08-31 01:25:07 -0300

    Configurate linter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.set("port", port)
+app.set('port', port)
 
 app.use(express.static(`${__dirname}/public`))
 
 // views is directory for all template files
-app.set("views", `${__dirname}/views`)
-app.set("view engine", "ejs")
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'ejs')
 
-app.listen(app.get("port"), () => {
+app.listen(app.get('port'), () => {
   console.log(`Node app is running on port ${port}`)
 })
--- test/index.js
+++ test/index.js
@@ -1,9 +1,10 @@
-import assert from "assert"
+import assert from 'assert'
+import { describe, it } from 'mocha'
 
-describe("Array", () => {
-  describe("#indexOf()", () => {
-    it("should return -1 when the value is not present", () => {
-      assert.equal(-1, [1,2,3].indexOf(4))
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
