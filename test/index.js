4571b96bcf056e4d65b96f53c9dc81e236353239
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-11 01:17:37 -0300

    Add firebase mock and add some getAllUsers tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,44 @@
-import assert from 'assert'
-import { describe, it } from 'mocha'
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import { describe, it, before, after } from 'mocha'
+import UserService from '../src/services/userService'
+import FirebaseServer from 'firebase-server'
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+describe('UserService', () => {
+  describe('#getAllUsers(uid)', () => {
+    let server
+    before(() => {
+      const users = {
+        users: {
+          'aUid': {
+            id: 'aUid'
+          },
+          'anotherUid': {
+            id: 'anotherUid'
+          }
+        }
+      }
+      server = new FirebaseServer(5000, 'localhost.firebaseio.test', users)
+    })
+
+    it('returns all users from database', () => {
+      return UserService().getAllUsers('someUid').then(users => {
+        expect(users.length).to.equal(2)
+      })
+    })
+
+    it('returns all users from database, except the one who is making the request', () => {
+      return UserService().getAllUsers('anotherUid').then(users => {
+        expect(users.length).to.equal(1)
+        expect(users[0].id).to.equal('aUid')
+      })
+    })
+
+    after(() => {
+      server.close(console.log('— firebase server closed -'))
     })
   })
 })
