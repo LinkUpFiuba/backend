e4e74b8d8f3a04c5a14c8cda0df6c41140e1d68a
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-17 13:08:34 -0300

    Tests use only the users they need

--- test/services/userService.test.js
+++ test/services/userService.test.js
@@ -2,20 +2,20 @@ import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import { describe, it, before, after } from 'mocha'
 import UserService from '../../src/services/userService'
-// import Database from '../../src/services/gateway/database'
+import Database from '../../src/services/gateway/database'
 import FirebaseServer from 'firebase-server'
 import { User } from './usersFactory'
 
 chai.use(chaiAsPromised)
 const expect = chai.expect
 
-// De 50 para arriba no importa el rango, los tests sobre la edad son por debajo de 50
 describe('UserService', () => {
   describe('#getPosibleLinks(uid)', () => {
     let server
     const maleForFriends = new User().male().likesFriends().get()
     const maleForFriends2 = new User().male().likesFriends().get()
     const femaleForFriends = new User().female().likesFriends().get()
+
     const maleForFemale = new User().male().likesFemale().get()
     const femaleForMale = new User().female().likesMale().get()
     const maleForMale = new User().male().likesMale().get()
@@ -23,11 +23,13 @@ describe('UserService', () => {
     const maleForMaleAndFemale = new User().male().likesMale().likesFemale().get()
     const femaleForFemale = new User().female().likesFemale().get()
     const femaleForMaleAndFemale = new User().female().likesMale().likesFemale().get()
+
     const femaleForMaleInAgeRange = new User().female().likesMale().age(30).ageRange(35, 45).get()
-    const maleForFemaleInAgeRange = new User().male().likesFemale().age(40).ageRange(25, 35).get()
+    const maleForFemaleInAgeRange = new User().male().likesFemale().age(35).ageRange(25, 35).get()
     const maleForFemaleInImposibleAgeRange = new User().female().likesMale().age(35).ageRange(60, 70).get()
-    const femaleForFemaleInvisibleMode = new User().female().likesFemale().invisible().get()
-    const femaleForFriendsInvisibleMode = new User().female().likesFriends().invisible().get()
+
+    const femaleForMaleInvisibleMode = new User().female().likesMale().invisible().get()
+
     const femaleForFriendsFarFromOthers = new User().female().likesFriends().withLocation(0, 0).get()
     const solariFemaleForFriends = new User().female().likesFemale().withLocation(1, 1).get()
     const solariFemaleForFriends2 = new User().female().likesFemale().withLocation(1, 1).get()
@@ -40,50 +42,20 @@ describe('UserService', () => {
     }
 
     before(() => {
-      const users = {
-        users: {
-          [maleForFriends.Uid]: maleForFriends,
-          [femaleForFriends.Uid]: femaleForFriends,
-          [maleForFemale.Uid]: maleForFemale,
-          [maleForFriends2.Uid]: maleForFriends2,
-          [femaleForMale.Uid]: femaleForMale,
-          [maleForMale.Uid]: maleForMale,
-          [maleForMale2.Uid]: maleForMale2,
-          [maleForMaleAndFemale.Uid]: maleForMaleAndFemale,
-          [femaleForFemale.Uid]: femaleForFemale,
-          [femaleForMaleAndFemale.Uid]: femaleForMaleAndFemale,
-          [femaleForMaleInAgeRange.Uid]: femaleForMaleInAgeRange,
-          [maleForFemaleInAgeRange.Uid]: maleForFemaleInAgeRange,
-          [maleForFemaleInImposibleAgeRange.Uid]: maleForFemaleInImposibleAgeRange,
-          [femaleForFemaleInvisibleMode.Uid]: femaleForFemaleInvisibleMode,
-          [femaleForFriendsInvisibleMode.Uid]: femaleForFriendsInvisibleMode,
-          [femaleForFriendsFarFromOthers.Uid]: femaleForFriendsFarFromOthers,
-          [solariFemaleForFriends.Uid]: solariFemaleForFriends,
-          [solariFemaleForFriends2.Uid]: solariFemaleForFriends2,
-          [solariFemaleForFemaleInPosition3.Uid]: solariFemaleForFemaleInPosition3,
-          [anotherSolariFemaleForFemaleInPosition3.Uid]: anotherSolariFemaleForFemaleInPosition3
-        }
-      }
-      server = new FirebaseServer(5000, 'localhost.firebaseio.test', users)
+      server = new FirebaseServer(5000, 'localhost.firebaseio.test', {})
     })
 
     describe('Search for friends', () => {
-      // let user
-      // before(() => {
-      //   const maleForFriends = new User().male().likesFriends().get()
-      //   console.log(maleForFriends)
-      //   const femaleForFriends = new User().female().likesFriends().get()
-      //   console.log(femaleForFriends)
-      //   const maleForFriends2 = new User().male().likesFriends().get()
-      //   user = maleForFriends
-      //   const users = {
-      //     [maleForFriends.Uid]: maleForFriends,
-      //     [femaleForFriends.Uid]: femaleForFriends,
-      //     [maleForFriends2.Uid]: maleForFriends2
-      //   }
-      //   const ref = Database('users')
-      //   ref.set(users)
-      // })
+      before(() => {
+        const users = {
+          [maleForFriends.Uid]: maleForFriends,
+          [femaleForFriends.Uid]: femaleForFriends,
+          [maleForFriends2.Uid]: maleForFriends2,
+          [maleForFemale.Uid]: maleForFemale
+        }
+        const ref = Database('users')
+        ref.set(users)
+      })
 
       it('returns all who search for friends, whatever sex', () => {
         return UserService().getPosibleLinks(maleForFriends.Uid).then(users => {
@@ -101,10 +73,25 @@ describe('UserService', () => {
     })
 
     describe('Search for people of one gender', () => {
+      before(() => {
+        const users = {
+          [maleForFemale.Uid]: maleForFemale,
+          [femaleForMale.Uid]: femaleForMale,
+          [maleForMale.Uid]: maleForMale,
+          [maleForMale2.Uid]: maleForMale2,
+          [femaleForFemale.Uid]: femaleForFemale,
+          [femaleForMaleAndFemale.Uid]: femaleForMaleAndFemale,
+          [maleForMaleAndFemale.Uid]: maleForMaleAndFemale
+        }
+        const ref = Database('users')
+        ref.set(users)
+      })
+
       it('male search for female', () => {
         return UserService().getPosibleLinks(maleForFemale.Uid).then(users => {
           expect(users.length).to.equal(2)
           expect(searchForUser(users, femaleForMale)).to.be.true
+          expect(searchForUser(users, femaleForMaleAndFemale)).to.be.true
         })
       })
 
@@ -112,6 +99,7 @@ describe('UserService', () => {
         return UserService().getPosibleLinks(maleForMale.Uid).then(users => {
           expect(users.length).to.equal(2)
           expect(searchForUser(users, maleForMale2)).to.be.true
+          expect(searchForUser(users, maleForMaleAndFemale)).to.be.true
         })
       })
 
@@ -119,6 +107,7 @@ describe('UserService', () => {
         return UserService().getPosibleLinks(maleForMale2.Uid).then(users => {
           expect(users.length).to.equal(2)
           expect(searchForUser(users, maleForMale)).to.be.true
+          expect(searchForUser(users, maleForMaleAndFemale)).to.be.true
         })
       })
 
@@ -134,7 +123,9 @@ describe('UserService', () => {
 
       it('female search for male', () => {
         return UserService().getPosibleLinks(femaleForMale.Uid).then(users => {
+          expect(users.length).to.equal(2)
           expect(searchForUser(users, maleForFemale)).to.be.true
+          expect(searchForUser(users, maleForMaleAndFemale)).to.be.true
         })
       })
 
@@ -156,6 +147,16 @@ describe('UserService', () => {
     })
 
     describe('Test age range', () => {
+      before(() => {
+        const users = {
+          [maleForFemaleInAgeRange.Uid]: maleForFemaleInAgeRange,
+          [femaleForMaleInAgeRange.Uid]: femaleForMaleInAgeRange,
+          [maleForFemaleInImposibleAgeRange.Uid]: maleForFemaleInImposibleAgeRange
+        }
+        const ref = Database('users')
+        ref.set(users)
+      })
+
       it('male search for female within range', () => {
         return UserService().getPosibleLinks(maleForFemaleInAgeRange.Uid).then(users => {
           expect(users.length).to.equal(1)
@@ -170,34 +171,60 @@ describe('UserService', () => {
       })
     })
 
-    // No se prueba que a los otros no les aparezcan, por que ya esta testeado arriba
     describe('Test for invisible mode', () => {
-      it('Female for female can search like always', () => {
-        return UserService().getPosibleLinks(femaleForFemaleInvisibleMode.Uid).then(users => {
-          expect(users.length).to.equal(2)
-          expect(searchForUser(users, femaleForMaleAndFemale)).to.be.true
+      before(() => {
+        const users = {
+          [femaleForMaleInvisibleMode.Uid]: femaleForMaleInvisibleMode,
+          [maleForFemale.Uid]: maleForFemale
+        }
+        const ref = Database('users')
+        ref.set(users)
+      })
+
+      it('Female for male can search like always', () => {
+        return UserService().getPosibleLinks(femaleForMaleInvisibleMode.Uid).then(users => {
+          expect(users.length).to.equal(1)
+          expect(searchForUser(users, maleForFemale)).to.be.true
         })
       })
 
-      it('Female for friends can search like always', () => {
-        return UserService().getPosibleLinks(femaleForFriendsInvisibleMode.Uid).then(users => {
-          expect(users.length).to.equal(3)
-          expect(searchForUser(users, femaleForFriends)).to.be.true
-          expect(searchForUser(users, maleForFriends)).to.be.true
-          expect(searchForUser(users, maleForFriends2)).to.be.true
+      it('Male for female cannot find female', () => {
+        return UserService().getPosibleLinks(maleForFemale.Uid).then(users => {
+          expect(users.length).to.equal(0)
         })
       })
     })
 
-    // No se prueba que a los otros no les aparezcan, por que ya esta testeado arriba
     describe('Test for distance filter', () => {
+      before(() => {
+        const users = {
+          [maleForFriends.Uid]: maleForFriends,
+          [femaleForFriendsFarFromOthers.Uid]: femaleForFriendsFarFromOthers
+        }
+        const ref = Database('users')
+        ref.set(users)
+      })
+
       it('Female for friends too far from others gets nothing', () => {
         return UserService().getPosibleLinks(femaleForFriendsFarFromOthers.Uid).then(users => {
           expect(users.length).to.equal(0)
         })
       })
 
+      it('Male for friends gets nothing', () => {
+        return UserService().getPosibleLinks(maleForFriends.Uid).then(users => {
+          expect(users.length).to.equal(0)
+        })
+      })
+
       it('Female for female search only one within the range (different locations)', () => {
+        const users = {
+          [solariFemaleForFemaleInPosition3.Uid]: solariFemaleForFemaleInPosition3,
+          [anotherSolariFemaleForFemaleInPosition3.Uid]: anotherSolariFemaleForFemaleInPosition3
+        }
+        const ref = Database('users')
+        ref.set(users)
+
         return UserService().getPosibleLinks(solariFemaleForFemaleInPosition3.Uid).then(users => {
           expect(users.length).to.equal(1)
           expect(searchForUser(users, anotherSolariFemaleForFemaleInPosition3)).to.be.true
@@ -205,6 +232,13 @@ describe('UserService', () => {
       })
 
       it('Solari female for friends only find one friend in same spot', () => {
+        const users = {
+          [solariFemaleForFriends.Uid]: solariFemaleForFriends,
+          [solariFemaleForFriends2.Uid]: solariFemaleForFriends2
+        }
+        const ref = Database('users')
+        ref.set(users)
+
         return UserService().getPosibleLinks(solariFemaleForFriends.Uid).then(users => {
           expect(users.length).to.equal(1)
           expect(searchForUser(users, solariFemaleForFriends2)).to.be.true
@@ -213,7 +247,7 @@ describe('UserService', () => {
     })
 
     after(() => {
-      server.close(console.log('— firebase server closed -'))
+      server.close(console.log('- Firebase server closed -'))
     })
   })
 })
