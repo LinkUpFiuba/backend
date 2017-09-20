ca5db7dd1203c1ef860823e5e2ed285754b3a220
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-20 00:09:16 -0300

    Add link check

--- src/services/linkService.js
+++ src/services/linkService.js
@@ -1,6 +1,11 @@
 import Database from './gateway/database'
 
 export default function LinkService() {
+  const checkLink = (linkingUser, linkedUser) => {
+    const linksRef = Database('links')
+    return linksRef.child(`${linkedUser}/${linkingUser}`).once('value').then(snapshot => snapshot.exists())
+  }
+
   return {
     getLinks: actualUser => {
       const linksRef = Database('links')
@@ -38,8 +43,9 @@ export default function LinkService() {
       // This works if we put a timestamp instead of a true for the links table
       linksRef.on('child_changed', link => {
         console.log('A child has changed!')
-        console.log(`Liking user: ${link.key}`)
-        let likedUsers = link.val()
+        const linkingUser = link.key
+        console.log(`Linking user: ${linkingUser}`)
+        let linkedUsers = link.val()
         // El problema que le veo a esto es que no se como se maneja firebase si entran varios changes.
         // O sea, si es posible que en el child_changed se genere cuando cambiaron 2 cosas casi simultaneas,
         // esto no funcionaria porque solo "atenderia" a la ultima. Si en cambio los eventos se "encolan"
@@ -48,8 +54,10 @@ export default function LinkService() {
         // Otra forma de solucionar esto, es tener un valor dentro de cada user que indique el ultimo
         // timestamp que proceso, y filtrar los timestamps mayores a este para procesar (y actualizarlo)
         // Order them by timestamp
-        likedUsers = Object.keys(likedUsers).sort((a, b) => likedUsers[b] - likedUsers[a])
-        console.log(`\tLiked user: ${likedUsers[0]}`)
+        linkedUsers = Object.keys(linkedUsers).sort((a, b) => linkedUsers[b] - linkedUsers[a])
+        const linkedUser = linkedUsers[0]
+        console.log(`\tLinked user: ${linkedUser}`)
+        console.log(`\tIs there a match? ${checkLink(linkingUser, linkedUser).then(value => console.log(value))}`)
       })
 
       // Listen to users that has never made a link, so the first time they will create their key in the db
@@ -57,11 +65,15 @@ export default function LinkService() {
       linksRef.on('child_added', link => {
         if (!newItems) return
         console.log('A child has been added!')
-        console.log(`Liking user: ${link.key}`)
+        const linkingUser = link.key
+        console.log(`Liking user: ${linkingUser}`)
         // Although we know there should be only one user here, we must do a forEach
+        let linkedUser
         link.forEach(child => {
-          console.log(`\tLiked user: ${child.key}`)
+          linkedUser = child.key
+          console.log(`\tLiked user: ${linkedUser}`)
         })
+        console.log(`\tIs there a match? ${checkLink(linkingUser, linkedUser).then(value => console.log(value))}`)
       })
       // This is in order not to trigger events when the server starts
       // See: https://stackoverflow.com/questions/18270995/how-to-retrieve-only-new-data
