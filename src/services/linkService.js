1c1d26d5f89a7b43f6d75f1872d091c7d8a1943e
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-19 21:25:48 -0300

    Basic link detection that does nothing when there's a new link

--- src/services/linkService.js
+++ src/services/linkService.js
@@ -29,6 +29,37 @@ export default function LinkService() {
     deleteUnlinks: actualUser => {
       const unlinksRef = Database('unlinks')
       return unlinksRef.child(actualUser.Uid).remove()
+    },
+
+    detectLinks: () => {
+      console.log('Starting to detect links')
+      const linksRef = Database('links')
+      // linksRef.on('child_changed', userSnapshot => {
+      //   console.log(userSnapshot.key, userSnapshot.val())
+      //   console.log(userSnapshot.toJSON())
+      //   userSnapshot.on('child_added', itemSnapshot => {
+      //     console.log(`${itemSnapshot.val()}`)
+      //   })
+      // })
+
+      // Esto funciona, pero no se puede determinar cual fue el usuario al cual likeo (trae todos ¯\_(ツ)_/¯)
+      linksRef.on('child_changed', link => {
+        console.log('A child has changed!')
+        console.log(`Liking user: ${link.key}`)
+        link.forEach(child => {
+          console.log(`\tPossible liked user: ${child.key}`)
+        })
+      })
+
+      // No se por que esto se ejecuta cuando arranca el server tambien, pero mas alla de eso, funca
+      linksRef.on('child_added', link => {
+        console.log('A child has been added!')
+        console.log(`Liking user: ${link.key}`)
+        // Although we know there should be only one user here, we must do a forEach
+        link.forEach(child => {
+          console.log(`\tLiked user: ${child.key}`)
+        })
+      })
     }
   }
 }
