1e1b020853f414ea4c6a7c5da703eb2109e34774
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-21 00:26:26 -0300

    Remove commented line

--- src/index.js
+++ src/index.js
@@ -54,7 +54,6 @@ app.post('/getToken', (request, response) => {
     .catch(error => response.json({ error: error }))
 })
 
-// LinkService().detectLinks()
 LinkService().detectLinks()
 
 app.listen(app.get('port'), () => {
