6e9a43a04a538c4bfeaa1d3ac539b6302ef513df
Pablo Rodrigo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-21 18:47:31 -0300

    Detectar links (#29)
    * Basic link detection that does nothing when there's a new link
    
    * Fix triggering  event when server starts
    
    * Make child_changed works with a little hack
    
    * Update comment
    
    * Add link check
    
    * Add new way of processing new possible matches
    
    * Create entry in /matches
    
    * Create both matches simultaneously
    
    * Remove unused possibility
    
    * Remove commented line
    
    * Change variable name
    
    * Add some tests, although they don't work yet
    
    * Extract onChildAdded function
    
    * Add tests for onChildAdded function
    
    * Remove unnecessary test
    
    * Remove comment

--- src/services/linkService.js
+++ src/services/linkService.js
@@ -1,6 +1,41 @@
 import Database from './gateway/database'
 
 export default function LinkService() {
+  const checkLink = (linkingUser, linkedUser) => {
+    const linksRef = Database('links')
+    return linksRef.child(`${linkedUser}/${linkingUser}`).once('value')
+      .then(snapshot => {
+        const isNewMatch = snapshot.exists()
+        // Create push notification!
+        console.log(`${isNewMatch ? '\tThere is a new match!' : '\tNo new match :('}`)
+        if (isNewMatch) {
+          const matchesRef = Database('matches')
+          const matchesToCreate = {}
+          matchesToCreate[`${linkedUser}/${linkingUser}/read`] = false
+          matchesToCreate[`${linkingUser}/${linkedUser}/read`] = false
+          return matchesRef.update(matchesToCreate).then(() => {
+            console.log('\tMatch successfully created!')
+          }).catch(() => {
+            console.log('\tMatch could not be created :(')
+          })
+        }
+      })
+  }
+
+  const onChildAdded = possibleMatch => {
+    const possibleMatchesRef = Database('possibleMatches')
+    console.log('A possible match was detected!')
+    const linkingUser = possibleMatch.child('linkingUser').val()
+    console.log(`\tLinking user: ${linkingUser}`)
+    const linkedUser = possibleMatch.child('linkedUser').val()
+    console.log(`\tLinked user: ${linkedUser}`)
+    return checkLink(linkingUser, linkedUser)
+      .then(() => possibleMatchesRef.child(possibleMatch.key).remove())
+      .then(() => {
+        console.log('Possible match removed!')
+      })
+  }
+
   return {
     getLinks: actualUser => {
       const linksRef = Database('links')
@@ -29,6 +64,20 @@ export default function LinkService() {
     deleteUnlinks: actualUser => {
       const unlinksRef = Database('unlinks')
       return unlinksRef.child(actualUser.Uid).remove()
+    },
+
+    // This is just for test purposes
+    onChildAdded: possibleMatch => {
+      return onChildAdded(possibleMatch)
+    },
+
+    detectLinks: () => {
+      console.log('Starting to detect links')
+      const possibleMatchesRef = Database('possibleMatches')
+
+      possibleMatchesRef.on('child_added', possibleMatch => {
+        onChildAdded(possibleMatch)
+      })
     }
   }
 }
