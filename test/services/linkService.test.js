2a4409ad680309a8c4ba5c9ef0898475a96f278b
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-21 02:25:01 -0300

    Add some tests, although they don't work yet

--- test/services/linkService.test.js
+++ test/services/linkService.test.js
@@ -91,4 +91,106 @@ describe('LinkService', () => {
       })
     })
   })
+
+  describe('#detectLinks()', () => {
+    const user1 = new User().get()
+    const user2 = new User().get()
+
+    describe('when only one user has linked', () => {
+      before(() => {
+        const links = {
+          [user1.Uid]: {
+            [user2.Uid]: true
+          }
+        }
+        const linksRef = Database('links')
+        linksRef.set(links)
+      })
+
+      it('does not create the match', () => {
+        const possibleMatch = {
+          aUniqueId: {
+            linkingUser: user1.Uid,
+            linkedUser: user2.Uid
+          }
+        }
+        const possibleMatchesRef = Database('possibleMatches')
+        const matchesRef = Database('matches')
+        possibleMatchesRef.set(possibleMatch)
+        return matchesRef.child(`aUniqueId/${user1.Uid}`).once('value').then(match => {
+          expect(match.exists()).to.be.false
+          expect(match.val()).to.be.null
+        })
+      })
+
+      // // This test should be also use when both users have linked
+      // it('removes the possible match', () => {
+      //   const possibleMatch = {
+      //     aUniqueId: {
+      //       linkingUser: user1.Uid,
+      //       linkedUser: user2.Uid
+      //     }
+      //   }
+      //   const possibleMatchesRef = Database('possibleMatches')
+      //   possibleMatchesRef.set(possibleMatch)
+      //   //   .then(() => {
+      //   //     return possibleMatchesRef.once('child_removed').then(possibleMatch => {
+      //   //       console.log('Child removed!')
+      //   //       expect(true).to.be.false
+      //   //       // expect(possibleMatch.key).to.equal('aUniqueId')
+      //   //     })
+      //   //   })
+      //
+      //   // Not working
+      //   // return possibleMatchesRef.once('child_removed').then(possibleMatch => {
+      //   //   console.log('Child removed!')
+      //   //   expect(possibleMatch.key).to.equal('aUniqueId')
+      //   // })
+      //
+      //   // Not working
+      //   // return setTimeout(() => {
+      //   //   console.log('In timeout')
+      //   //   return possibleMatchesRef.once('value').then(possibleMatches => {
+      //   //     expect(possibleMatches.val()).to.be.null
+      //   //   })
+      //   // }, 5000)
+      //
+      //   // Working but needs to wait until it's removed to pass the expect
+      //   // return possibleMatchesRef.once('value').then(possibleMatches => {
+      //   //   expect(possibleMatches.val()).to.be.null
+      //   // })
+      // })
+    })
+
+    describe('when both users have linked each other', () => {
+      before(() => {
+        const links = {
+          [user1.Uid]: {
+            [user2.Uid]: true
+          },
+          [user2.Uid]: {
+            [user1.Uid]: true
+          }
+        }
+        const linksRef = Database('links')
+        linksRef.set(links)
+      })
+
+      // it('creates the match', () => {
+      //   const possibleMatch = {
+      //     aUniqueId: {
+      //       linkingUser: user1.Uid,
+      //       linkedUser: user2.Uid
+      //     }
+      //   }
+      //   const possibleMatchesRef = Database('possibleMatches')
+      //   const matchesRef = Database('matches')
+      //   return possibleMatchesRef.set(possibleMatch).then(() => {
+      //     return matchesRef.once('child_added').then(matches => {
+      //       expect(matches.val()).not.to.be.null
+      //     })
+      //   })
+      // })
+    })
+  })
 })
