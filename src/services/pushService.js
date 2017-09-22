fa90c8a2facffa53d059e4925b986e2876f62610
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-09-22 00:59:20 -0300

    Add personalized push notification for each user

--- src/services/pushService.js
+++ src/services/pushService.js
@@ -2,36 +2,45 @@ import Database from './gateway/database'
 import { Messaging } from './gateway/messaging'
 
 export const PushNotificationService = () => {
-  const getTokenFCM = user => {
+  const getUser = user => {
     const usersRef = Database('users')
-    return usersRef.child(`${user}/tokenFCM`).once('value').then(token => {
-      return token.val()
+    return usersRef.child(`${user}`).once('value').then(user => {
+      return user.val()
     })
+  }
+
+  const sendMatchPush = (user1, user2) => {
+    const payload = {
+      notification: {
+        title: 'Nuevo link!',
+        body: `${user2.name} también quiere linkear con vos! Chateale! 😉`
+      },
+      data: {
+        name: user2.name,
+        photo: user2.photoUrl
+      }
+    }
+    return Messaging().sendToDevice(user1.tokenFCM, payload)
+      .then(response => {
+        console.log(`Successfully sent message to user ${user1.Uid}:`, response)
+      })
+      .catch(error => {
+        console.log(`Error sending message to user ${user1.Uid}:`, error)
+      })
   }
 
   return {
     sendMatchPush: (user1, user2) => {
-      const payload = {
-        notification: {
-          title: 'Nuevo match!',
-          body: 'Esta es una push notification creada desde el servidor'
-        }
-      }
-      let token1
-      let token2
-      return getTokenFCM(user1).then(token => {
-        token1 = token
-        return getTokenFCM(user2)
-      }).then(token => {
-        token2 = token
-        const tokens = [token1, token2]
-        return Messaging().sendToDevice(tokens, payload)
-          .then(response => {
-            console.log('Successfully sent message:', response)
+      let userOne
+      let userTwo
+      return getUser(user1).then(user => {
+        userOne = user
+        return getUser(user2).then(user => {
+          userTwo = user
+          return sendMatchPush(userOne, userTwo).then(() => {
+            return sendMatchPush(userTwo, userOne)
           })
-          .catch(error => {
-            console.log('Error sending message:', error)
-          })
+        })
       })
     }
   }
