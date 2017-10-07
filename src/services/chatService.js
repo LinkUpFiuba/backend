9d975576e08ed7419acd4b00669af71ed5943f21
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-10-07 11:14:14 -0300

    Code cleanup

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -5,60 +5,37 @@ export const ChatService = () => {
   const sendPush = (user1, user2, message) => {
     if (message.userId !== user1) {
       console.log(`\t\tSending new message "${message.message}" push notification to ${user1} from ${user2}`)
-      console.log("PUSH!\n\n")
       // PushNotificationService().sendNewMessagePush(user1, user2, message)
     }
   }
 
-  // This method is called when the server starts, in order to set all listeners. There are 3 possibilities:
+  // This method is called when the server starts, in order to set all listeners. When it's starting, it
+  // should not send push notifications. There are 3 possibilities:
   //   1 - The user has never chatted before (It's his first chat)
   //   2 - The user has another chats, but with another users
   //   3 - The user has already a chat with that user
-  const onNewFirstMessage = messagesRef => {
+  const onNewMessage = messagesRef => {
     let newMessages = false
     // Set the listener on first chat of a user (1)
-    messagesRef.on('child_added', chat => {
-      const user1 = chat.key
-      console.log(`1 - A user (${user1}) that had never chated before has a new chat!`)
+    messagesRef.on('child_added', newChatingUser => {
+      const user1 = newChatingUser.key
+      console.log(`1 - ${user1} has started chating!`)
 
-      // Set the listener on first chat with that user, having others before (2)
-      messagesRef.child(user1).on('child_added', asd => {
-        const user2 = asd.key
-        console.log(`\t2 - A user (${user1}) that had already chated (with other users) has a new chat with ${user2}!`)
+      // Set the listener on new chats for user1 (2)
+      messagesRef.child(user1).on('child_added', newChat => {
+        const user2 = newChat.key
+        console.log(`\t2 - ${user1} has a new chat with ${user2}!`)
 
         const childRef = messagesRef.child(`${user1}/${user2}`)
-        childRef.on('child_added', message => {
-          // if (messages.numChildren() === 1) {
-          // Although we know there's only one child, we must do a for each as we don't know the keys
-          // messages.forEach(message => {
-            const realMessage = message.val()
-            if (message.key !== 'blocked') {
-              console.log(`\t\t2 - New message "${realMessage.message}" on new chat from ${realMessage.userId} to ${realMessage.userId !== user1 ? user1 : user2}`)
-              if (newMessages) sendPush(user1, user2, realMessage)
-            }
-          // })
-          // }
+        // Set the listener on new messages between user1 and user2 (3)
+        childRef.on('child_added', newMessage => {
+          const message = newMessage.val()
+          if (newMessage.key !== 'blocked') {
+            console.log(`\t\t3 - New message "${message.message}" from ${message.userId} to ${message.userId !== user1 ? user1 : user2}`)
+            if (newMessages) sendPush(user1, user2, message)
+          }
         })
       })
-
-      // Esto no se debería hacer sólo cuando arranca?
-      // if (!newMessages) {
-      //   chat.forEach(message => {
-      //     const user2 = message.key
-      //     const childRef = messagesRef.child(`${user1}/${user2}`)
-      //
-      //     // Set the listener on already existing chat (3)
-      //     childRef.on('child_added', realMessage => {
-      //       const message = realMessage.val()
-      //       if (realMessage.key !== 'blocked') {
-      //         // Este console se puede mover adentro del if
-      //         console.log(`\t\t3 - A new message has arrive from two users (${user1} and ${user2}) that were chating: "${message.message}" from ${message.userId} to ${message.userId !== user1 ? user1 : user2}`)
-      //         // Acá no se deberían mandar pushes
-      //         if (newMessages) sendPush(user1, user2, message)
-      //       }
-      //     })
-      //   })
-      // }
     })
 
     // This is in order not to trigger events when the server starts
@@ -72,7 +49,7 @@ export const ChatService = () => {
     detectNewMessages: () => {
       console.log('Starting to detect new chat messages')
       const messagesRef = Database('messages')
-      onNewFirstMessage(messagesRef)
+      onNewMessage(messagesRef)
     }
   }
 }
