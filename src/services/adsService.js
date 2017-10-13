8f030ba935ce9f220b10fb876dca69d5972be74d
fedebrasburg <federico.brasburg@gmail.com>
2017-10-13 13:40:04 -0300

    Agrego endpoint para obtener las publicidades (#49)

---
+++ src/services/adsService.js
@@ -0,0 +1,15 @@
+import Database from './gateway/database'
+
+export default function AdsService() {
+  return {
+    getAllAds: () => {
+      const adsRef = Database('ads')
+      const adsArray = []
+      return adsRef.once('value').then(ads => {
+        ads.forEach(ad => {
+          adsArray.push(ad.val())
+        })
+      }).then(() => adsArray)
+    }
+  }
+}
---
+++ test/services/adsService.test.js
@@ -0,0 +1,62 @@
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import { describe, before, it } from 'mocha'
+import Database from '../../src/services/gateway/database'
+import AdsService from '../../src/services/adsService'
+import { Ad } from './adsFactory'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+describe('complaintService', () => {
+  describe('getAllAds', () => {
+    const googleAd = new Ad('Google', 'Google image').get()
+    const facebookAd = new Ad('Facebook', 'Facebook image').get()
+
+    before(() => {
+      Database('ads').set({})
+    })
+
+    describe('when there is zero ads', () => {
+      it('should return non ad', () => {
+        return AdsService().getAllAds().then(ads => {
+          expect(ads.length).to.equal(0)
+        })
+      })
+    })
+
+    describe('when there is one ad', () => {
+      before(() => {
+        const ads = {
+          [googleAd.id]: googleAd
+        }
+        Database('ads').set(ads)
+      })
+
+      it('should return one ad', () => {
+        return AdsService().getAllAds().then(ads => {
+          expect(ads.length).to.equal(1)
+          expect(ads[0].id).to.equal(googleAd.id)
+        })
+      })
+
+      describe('when there are two ads', () => {
+        before(() => {
+          const ads = {
+            [googleAd.id]: googleAd,
+            [facebookAd.id]: facebookAd
+          }
+          Database('ads').set(ads)
+        })
+
+        it('should return one ad', () => {
+          return AdsService().getAllAds().then(ads => {
+            expect(ads.length).to.equal(2)
+            expect(ads[0].id).to.equal(googleAd.id)
+            expect(ads[1].id).to.equal(facebookAd.id)
+          })
+        })
+      })
+    })
+  })
+})
